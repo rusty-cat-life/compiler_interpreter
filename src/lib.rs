c0ce01565aa23241376ccb645d138703c0ad93d4
rustycat <rustycat@cat.com>
2019-07-23 14:10:43 +0900

    wip

--- src/lib.rs
+++ src/lib.rs
@@ -366,31 +366,24 @@ fn parse_expr2<Tokens>(tokens: &mut Peekable<Tokens>) -> Result<Ast, ParseError>
 where
     Tokens: Iterator<Item = Token>,
 {
-    let mut e = parse_expr1(tokens)?;
-
-    loop {
-        match tokens.peek().map(|token| token.value) {
-            Some(TokenKind::Asterisk) | Some(TokenKind::Slash) => {
-                let operator = match tokens.next().unwrap() {
-                    Token {
-                        value: TokenKind::Asterisk,
-                        loc,
-                    } => BinOp::mult(loc),
-                    Token {
-                        value: TokenKind::Slash,
-                        loc,
-                    } => BinOp::div(loc),
-                    _ => unreachable!(),
-                };
-
-                let r = parse_expr1(tokens)?;
-                let loc = e.loc.merge(&r.loc);
-
-                e = Ast::binop(operator, e, r, loc);
-            }
-            _ => return Ok(e),
-        }
+    // `parse_left_binop` に渡す関数を定義する
+    fn parse_expr2_op<Tokens>(tokens: &mut Peekable<Tokens>) -> Result<BinOp, ParseError>
+    where
+        Tokens: Iterator<Item = Token>,
+    {
+        let op = tokens
+            .peek()
+            .ok_or(ParseError::Eof)
+            .and_then(|tok| match tok.value {
+                TokenKind::Asterisk => Ok(BinOp::mult(tok.loc.clone())),
+                TokenKind::Slash => Ok(BinOp::div(tok.loc.clone())),
+                _ => Err(ParseError::NotOperator(tok.clone())),
+            })?;
+        tokens.next();
+        Ok(op)
     }
+
+    parse_left_binop(tokens, parse_expr1, parse_expr2_op)
 }
 
 fn parse_expr1<Tokens>(tokens: &mut Peekable<Tokens>) -> Result<Ast, ParseError>
